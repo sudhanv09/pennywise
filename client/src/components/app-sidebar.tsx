4acfb216c66fe7f30444e4dff0f29c87ab19cf7c
sudhanv09 <mystogun125@gmail.com>
2025-03-19 13:49:59 +0800

    add: basic sidebar

---
+++ client/src/components/app-sidebar.tsx
@@ -0,0 +1,75 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarGroup,
+  SidebarGroupContent,
+} from "@/components/ui/sidebar";
+import { Clock, Landmark, LayoutDashboardIcon, Settings } from "lucide-react";
+
+const data = [
+  {
+    title: "Dashboard",
+    icon: LayoutDashboardIcon,
+    url: "/",
+  },
+  {
+    title: "Activity",
+    icon: Clock,
+    url: "/log",
+  },
+  {
+    title: "Accounts",
+    icon: Landmark,
+    url: "/accounts",
+  },
+  {
+    title: "Settings",
+    icon: Settings,
+    url: "/settings",
+  },
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+            <SidebarMenu>
+              {data.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild tooltip={item.title}>
+                    <a href={item.url}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+}
--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,8 +1,20 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+
+        </SidebarInset>
+
+      </SidebarProvider>
       <Outlet />
     </>
   ),
