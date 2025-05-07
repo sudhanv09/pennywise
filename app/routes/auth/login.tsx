5c87ba652baf74ae601be28cabafb568da5aaef2
sudhanv09 <mystogun125@gmail.com>
2025-05-07 11:36:25 +0800

    auth cards styling complete

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -15,16 +15,16 @@ function RouteComponent() {
         </div>
         <form action="" class={styles.card__body}>
           <div class={styles.input_group}>
-            <input id="email" type="email" placeholder="john.doe@email.com" required/>
+            <input id="email" type="email" required />
             <label for="email">Email</label>
           </div>
           <div class={styles.input_group}>
-            <input id="password" type="password" required/>
+            <input id="password" type="password" required />
             <label for="password">Password</label>
           </div>
-          <a href="#">Forgot Password?</a>
           <button>Submit</button>
         </form>
+        <a href="#">Forgot Password?</a>
         <div class={styles.card__footer}>
           <p>
             Don't have an account? <a href="/auth/sign-up">Sign up</a>
