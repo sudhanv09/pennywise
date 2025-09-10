b5049af8b6ee786bfa23d6d144d74b1def9fb259
sudhanv09 <mystogun125@gmail.com>
2025-09-10 17:37:22 +0800

    fix env, add prisma config

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "prisma/config"
+import path from "node:path";
+
+export default defineConfig({
+    schema: path.join("prisma", "schema.prisma"),
+    migrations: { path: path.join("prisma", "migrations") },
+})
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,3 +1,9 @@
+import { env } from "$env/dynamic/private"
 import { PrismaClient } from "@/generated/prisma/client"
-
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: env.DATABASE_URL
+        }
+    }
+})
