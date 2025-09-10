ac193e3a2d93f948a713510c3bab89cc4b5f3969
sudhanv09 <mystogun125@gmail.com>
2025-09-10 15:24:14 +0800

    app sidebar

---
+++ src/lib/components/app/app-sidebar.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import type { ComponentProps } from "svelte";
+
+  import House from "@lucide/svelte/icons/house";
+  import SquareActivity from "@lucide/svelte/icons/square-activity";
+  import Goal from "@lucide/svelte/icons/goal";
+  import Landmark from "@lucide/svelte/icons/landmark";
+  import Handcoins from "@lucide/svelte/icons/hand-coins";
+  import CalendarCheck2 from "@lucide/svelte/icons/calendar-check-2";
+  import CirclePoundSterling from "@lucide/svelte/icons/circle-pound-sterling";
+
+  const data = {
+    main: [
+      {
+        title: "Dashboard",
+        url: "#",
+        icon: House,
+      },
+      {
+        title: "Activity",
+        url: "#",
+        icon: SquareActivity,
+      },
+    ],
+    user: [
+      {
+        title: "Accounts",
+        url: "#",
+        icon: Landmark,
+      },
+      {
+        title: "Goals",
+        url: "#",
+        icon: Goal,
+      },
+      {
+        title: "Loan",
+        url: "#",
+        icon: Handcoins,
+      },
+      {
+        title: "Subscriptions",
+        url: "#",
+        icon: CalendarCheck2,
+      },
+    ],
+  };
+  let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root collapsible="offcanvas" {...restProps}>
+  <Sidebar.Header>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:!p-1.5">
+          {#snippet child({ props })}
+            <a href="##" {...props}>
+              <CirclePoundSterling class="!size-5" />
+              <span class="text-base font-semibold">Pennywise</span>
+            </a>
+          {/snippet}
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Header>
+  <Sidebar.Content>
+    <Sidebar.Group>
+      <Sidebar.GroupContent class="flex flex-col gap-2">
+        <Sidebar.Menu>
+          {#each data.main as item (item.title)}
+            <Sidebar.MenuItem>
+              <Sidebar.MenuButton tooltipContent={item.title}>
+                {#if item.icon}
+                  <item.icon />
+                {/if}
+                <span>{item.title}</span>
+              </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+          {/each}
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>User</Sidebar.GroupLabel>
+      <Sidebar.GroupContent class="flex flex-col gap-2">
+        <Sidebar.Menu>
+          {#each data.user as item (item.title)}
+            <Sidebar.MenuItem>
+              <Sidebar.MenuButton tooltipContent={item.title}>
+                {#if item.icon}
+                  <item.icon />
+                {/if}
+                <span>{item.title}</span>
+              </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+          {/each}
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+  </Sidebar.Content>
+  <!-- <Sidebar.Footer>
+    <NavUser user={data.user} />
+  </Sidebar.Footer> -->
+</Sidebar.Root>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import AppSidebar from "$lib/components/app/app-sidebar.svelte";
+  import { Separator } from "$lib/components/ui/separator/index.js";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+</script>
+
+<Sidebar.Provider>
+  <AppSidebar variant="inset" />
+  <Sidebar.Inset>
+    <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+      <Sidebar.Trigger class="-ml-1" />
+      <Separator orientation="vertical" class="mr-2 h-4" />
+    </header>
+  </Sidebar.Inset>
+</Sidebar.Provider>
