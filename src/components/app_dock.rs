81e60c14ef268dda86d0060696e6c58cd0e7fd41
sudhanv09 <mystogun125@gmail.com>
2026-03-06 16:04:13 +0800

    minor

--- src/components/app_dock.rs
+++ src/components/app_dock.rs
@@ -1,14 +1,15 @@
 use dioxus::prelude::*;
+use crate::Route;
 
 #[component]
 pub fn AppDock() -> Element {
     rsx! {
         div {
             id: "appdock",
-            ul { 
-                li { "Home" },
-                li { "Transactions" },
-                li { "Other" },
+            ul {
+                li { Link { to: Route::Home, "Home" } }
+                li { Link { to: Route::Transactions, "Transactions" } }
+                li { Link { to: Route::Settings, "Settings" } }
             }
         }
     }
